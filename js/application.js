6886819e04c25ccf003236fc09d00dd6981da078
jeffamed <jeffamed@gmail.com>
2019-05-27 22:41:36 -0600

    se agrego iconos al menu ahora se vera el datepicker hacer responsive

--- js/application.js
+++ js/application.js
@@ -3,6 +3,7 @@ $(function(){
    $(window).resize(function(){
       var anchop = $(window).width();
       console.log(anchop);
+
       if(anchop<=768){
          $("#contenido-enc").addClass("justify-content-between");
       } else{
@@ -19,6 +20,26 @@ $(function(){
          $("#logo").removeClass("justify-content-between");
          $("#pagination").removeClass("justify-content-center");
          $("#pagination").addClass("justify-content-end");
+      }
+
+      if(anchop<=575){
+         $("#acordion").removeClass("justify-content-center");
+         $("#acordion").addClass("align-content-start");
+      }else{
+         $("#acordion").removeClass("align-content-start");
+         $("#acordion").addClass("justify-content-center");
+      }
+
+      if(anchop<=544){
+         $("#select-opciones").removeClass("mr-1 w-25");
+         $("#txtbuscar").removeClass("w-50");
+         $("#select-opciones").addClass("w-100");
+         $("#txtbuscar").addClass("w-100");
+      }else{
+         $("#select-opciones").removeClass("w-100");
+         $("#txtbuscar").removeClass("w-100");
+         $("#select-opciones").addClass("mr-1 w-25");
+         $("#txtbuscar").addClass("w-50");
       }
    });
 
